d0685979a489df615b81c530a96dc7b53e83647e
Siddhantsingh1230 <Siddhantsingh9023134084@gmail.com>
2023-08-19 22:20:48 +0530

    added update page /post page

--- src/components/Upload.jsx
+++ src/components/Upload.jsx
@@ -1,11 +1,26 @@
-import React from 'react';
+import React from "react";
 
 const Upload = () => {
   return (
     <>
-      Upload
+      <div className="uploadContainer">
+        <div className="uploadTitle">
+          Create Tale <span>.</span>
+        </div>
+        <div className="uploadBox">
+        <i className="ri-upload-2-line"></i>
+        <p>Drag & Drop or <span>Choose file</span> to upload</p>
+        <p>img or vid</p>
+        </div>
+        <div className="captionInput">
+          <input type="search" placeholder="Add caption"/>
+        </div>
+        <div className="uploadAction">
+          <button>Post</button>
+        </div>
+      </div>
     </>
   );
-}
+};
 
 export default Upload;
