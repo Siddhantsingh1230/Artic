cfa5f3dcb4f41dfb9704d1b12c428142e1138ccf
Siddhantsingh1230 <Siddhantsingh9023134084@gmail.com>
2023-08-19 22:43:58 +0530

    added spinner element

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,15 +1,13 @@
-import React from "react";
+import React from 'react';
 
 const Loader = () => {
   return (
     <>
       <div className="loader">
-        <svg viewBox="25 25 50 50">
-          <circle r="20" cy="50" cx="50"></circle>
-        </svg>
+        <p>Loading..</p>
       </div>
     </>
   );
-};
+}
 
 export default Loader;
