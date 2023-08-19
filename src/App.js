86cc878754b3146fc61e5a9ee7317c5344096de7
Siddhantsingh1230 <Siddhantsingh9023134084@gmail.com>
2023-08-19 16:20:08 +0530

    added Loader

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 import Home from "./components/Home";
+import Loader from "./components/Loader";
+
 const App = () => {
-  return (
-    <>
-      <Home/>
-    </>
-  );
-}
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    window.onload = () => {
+      setIsLoading(false); // When everything is loaded, set isLoading to false
+    };
+  }, []);
+  return <>{isLoading ? <Loader /> : <Home />}</>;
+};
 
 export default App;
