c8f0bf93129f77039a17149db88444b8177a6bea
Siddhantsingh1230 <Siddhantsingh9023134084@gmail.com>
2023-08-20 10:27:48 +0530

    added setting page

--- src/components/Setting.jsx
+++ src/components/Setting.jsx
@@ -1,11 +1,48 @@
-import React from 'react';
+import React, { useState } from "react";
 
 const Setting = () => {
+  const [name, setName] = useState("Dimitri");
+  const [email, setEmail] = useState("Dimitri1230@gmail.com");
+  const handleNameChange=(e)=>{
+    setName(e.target.value);
+  }
+  const handleEmailChange=(e)=>{
+    setEmail(e.target.value);
+  }
   return (
     <>
-      Settings
+      <div className="setting">
+        <h2>Public Profile</h2>
+        <hr />
+        <div className="info">
+          <div className="userDetails">
+            <p>Name</p>
+            <div className="userSettingName">
+              <i className="ri-user-line"></i>
+              <input onChange={(e)=>{
+                handleNameChange(e);
+              }} value={name} type="text"/>
+            </div>
+            <small>
+              Your name may appear around Artic wherever you contribute or are
+              mentioned.
+            </small>
+            <p>Public Email</p>
+            <div className="userSettingEmail">
+              <i class="ri-verified-badge-line"></i>
+              <input onChange={(e)=>{
+                handleEmailChange(e);
+              }} value={email} type="text"/>
+            </div>
+            <small>Your verified email.</small>
+          </div>
+          <div className="userImage" ><img src="icon/userSprite.jpg" alt="" /></div>
+        </div>
+        <button className="save">Update</button>
+        <button className="delete">Delete</button>
+      </div>
     </>
   );
-}
+};
 
 export default Setting;
