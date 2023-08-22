4eab99598d959ab0fc185f306bcd7f925eb62a82
siddhu1919 <siddharthsingh1071@gmail.com>
2023-08-22 20:23:02 +0530

    added tooltip in setting userImage

--- src/components/Setting.jsx
+++ src/components/Setting.jsx
@@ -2,9 +2,13 @@ import React, { useState } from "react";
 
 const Setting = () => {
   const [name, setName] = useState("Dimitri");
+  const [lastName, setLastName] = useState("Machovich");
   const [email, setEmail] = useState("Dimitri1230@gmail.com");
   const handleNameChange=(e)=>{
     setName(e.target.value);
+  }
+  const handleLastNameChange=(e)=>{
+    setLastName(e.target.value);
   }
   const handleEmailChange=(e)=>{
     setEmail(e.target.value);
@@ -16,7 +20,7 @@ const Setting = () => {
         <hr />
         <div className="info">
           <div className="userDetails">
-            <p>Name</p>
+            <p>First name</p>
             <div className="userSettingName">
               <i className="ri-user-line"></i>
               <input onChange={(e)=>{
@@ -27,16 +31,28 @@ const Setting = () => {
               Your name may appear around Artic wherever you contribute or are
               mentioned.
             </small>
+            <p>Last name</p>
+            <div className="userSettingName">
+              <i className="ri-user-line"></i>
+              <input onChange={(e)=>{
+                handleLastNameChange(e);
+              }} value={lastName} type="text"/>
+            </div>
+            <small>
+              Your first name and last name make up your identity across Articverse.
+            </small>
             <p>Public Email</p>
             <div className="userSettingEmail">
-              <i class="ri-verified-badge-line"></i>
+              <i className="ri-verified-badge-line"></i>
               <input onChange={(e)=>{
                 handleEmailChange(e);
               }} value={email} type="text"/>
             </div>
             <small>Your verified email.</small>
           </div>
-          <div className="userImage" ><img src="icon/userSprite.jpg" alt="" /></div>
+          <div className="userImage" ><img src="icon/userSprite.jpg" alt="" />
+          <div className="tooltip2">{name +" "+ lastName}</div>
+          </div>
         </div>
         <button className="save">Update</button>
         <button className="delete">Delete</button>
