36375251ef1fe4ca9154bf0bb0b60ce88b4b0d58
Siddhantsingh1230 <Siddhantsingh9023134084@gmail.com>
2023-08-24 20:21:52 +0530

    modified styles

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -1,16 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+import Popup from "./Popup";
 
 const PostCard = () => {
+  const [renderPopup, setRenderPopup] = useState(false);
   return (
     <>
       <div className="postContainer ">
-        <div className="postMedia"><img src="icon/tale.jpg" alt="postMedia" /></div>
+        {renderPopup ? <Popup setRender={setRenderPopup} /> : null}
+        <div className="postMedia">
+          <img src="icon/tale.jpg" alt="postMedia" />
+        </div>
         <div className="postAction">
-          <button className="edit-button">
-          <i className="fa-solid fa-pen" style={{color: "#ffffff",}} />
+          <button
+            onClick={() => {
+              setRenderPopup(true);
+            }}
+            className="view-button"
+          >
+            <i className="fa-regular fa-eye" />
           </button>
           <button className="delete-button">
-          <i className="fa-solid fa-trash" style={{color: "#ffffff",}} />
+            <i className="fa-solid fa-trash" style={{ color: "#ffffff" }} />
           </button>
         </div>
       </div>
