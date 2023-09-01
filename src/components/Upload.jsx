8cec0ac753ab65ce189eb05beea046460f328a72
Siddhantsingh1230 <Siddhantsingh9023134084@gmail.com>
2023-09-02 03:02:11 +0530

    making ui for tales upload

--- src/components/Upload.jsx
+++ src/components/Upload.jsx
@@ -1,23 +1,37 @@
 import React from "react";
 
 const Upload = () => {
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log("submitted");
+  };
   return (
     <>
       <div className="uploadContainer">
         <div className="uploadTitle">
           Publish Tale <span>.</span>
         </div>
-        <div className="uploadBox">
-        <i className="ri-upload-2-line"></i>
-        <p>Drag & Drop or <span>Choose file</span> to upload</p>
-        <p>img or vid</p>
-        </div>
-        <div className="captionInput">
-          <input type="search" placeholder="Add caption"/>
-        </div>
-        <div className="uploadAction">
-          <button>Post</button>
-        </div>
+        <form onSubmit={submitHandler}>
+          <label className="uploadBox">
+            <i className="ri-upload-2-line"></i>
+            <p>
+              Drag & Drop or <span>Choose file</span> to upload
+              <input
+                type="file"
+                name="posts"
+                accept="image/*"
+                required
+              />
+            </p>
+            <p>img or vid</p>
+          </label>
+          <div className="captionInput">
+            <input type="search" placeholder="Add caption" />
+          </div>
+          <div className="uploadAction">
+            <button type="submit">Post</button>
+          </div>
+        </form>
       </div>
     </>
   );
