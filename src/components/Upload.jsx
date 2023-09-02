a6283f47bebb7dd254eb155b5a176ac153483a3c
Siddhantsingh1230 <Siddhantsingh9023134084@gmail.com>
2023-09-02 12:26:51 +0530

    modified Upload.jsx

--- src/components/Upload.jsx
+++ src/components/Upload.jsx
@@ -1,19 +1,63 @@
-import React, { useState } from "react";
+import React, { useState ,useContext } from "react";
+import axios from "axios";
+import { serverURI } from "../App";
+import { toast } from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
+import Spinner from "./Spinner";
+import { Context } from "../index";
+
+
 
 const Upload = () => {
+  const [previewURL, setPreviewURL] = useState("");
+  const {user} = useContext(Context);
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [caption,setCaption] =useState("");
+  const navigate = useNavigate();
+  const setPreview = (e) => {
+    const [file] = e.target.files;
+    setSelectedFile(e.target.files[0]);
+    if (file) {
+      setPreviewURL(URL.createObjectURL(file));
+    } else {
+      setPreviewURL("");
+    }
+  };
+
   const submitHandler = (e) => {
     e.preventDefault();
-    console.log("submitted");
+    setLoading(true);
+    if (!selectedFile) {
+      toast.error("No image selected");
+      return;
+    }
+    const formData = new FormData();
+    formData.append("posts", selectedFile);
+    formData.append("postCaption",caption);
+    formData.append("userID",user._id);
+    axios
+      .post(`${serverURI}/posts/upload`, formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+        withCredentials: true,
+      })
+      .then((response) => {
+        toast.success(response.data.message);
+        setLoading(false);
+        navigate("/");
+      })
+      .catch((error) => {
+        setLoading(false);
+        if (error.response) {
+          toast.error(error.response.data.message);
+        } else {
+          toast.error("Request failed");
+        }
+      });
   };
-  const [previewURL,setPreviewURL] =useState("");
-const setPreview = (e) =>{
-  const [file] = e.target.files;
-  if(file){
-    setPreviewURL(URL.createObjectURL(file));
-  }else{
-    setPreviewURL("");
-  }
-}
+
   return (
     <>
       <div className="uploadContainer">
@@ -26,12 +70,18 @@ const setPreview = (e) =>{
               <i className="ri-upload-2-line"></i>
               <p>
                 Drag & Drop or <span>Choose file</span> to upload
-                <input onChange={setPreview} type="file" name="posts" accept="image/*" required />
+                <input
+                  onChange={setPreview}
+                  type="file"
+                  name="posts"
+                  accept="image/*"
+                  required
+                />
               </p>
               <p>img or vid</p>
             </label>
             <div className="captionInput">
-              <input type="search" placeholder="Add caption" />
+              <input value={caption} onChange={(e)=>setCaption(e.target.value)} type="search" placeholder="Add caption" />
             </div>
             <div className="uploadAction">
               <button type="submit">Post</button>
@@ -39,14 +89,17 @@ const setPreview = (e) =>{
           </form>
         </div>
         <div className="uploadSection2">
-        <div className="uploadTitle">
-            Preview 
-          </div>
+          <div className="uploadTitle">Preview</div>
           <div className="uploadPreview">
-          {previewURL?<img src={previewURL} alt="" className="imgPreview" />:<p>Preview</p>}
+            {previewURL ? (
+              <img src={previewURL} alt="" className="imgPreview" />
+            ) : (
+              <p>Preview</p>
+            )}
           </div>
         </div>
       </div>
+      {loading ? <Spinner /> : null}
     </>
   );
 };
