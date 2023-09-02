4f5c44db85d9935adbd1e6634ece90ef3de04965
Siddhantsingh1230 <Siddhantsingh9023134084@gmail.com>
2023-09-02 22:07:54 +0530

    modified no posts and added style

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -3,6 +3,7 @@ import PostCard from "./PostCard";
 import axios from "axios";
 import { serverURI } from "../App";
 import Spinner from "./Spinner";
+import { Link } from "react-router-dom";
 
 const Posts = () => {
   const [posts,setPosts]=useState([]);
@@ -30,7 +31,7 @@ const Posts = () => {
   return (
     <>
       <div className="posts">
-        {posts.map((item) => <PostCard post={item} key={item._id} />)}
+        {posts.length>0?(posts.map((item) => <PostCard post={item} key={item._id} />)):(<div className="noPosts"><p>No Posts 💔<Link>+</Link></p></div>)}
       </div>
       {loading ? <Spinner /> : null}
     </>
