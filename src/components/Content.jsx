d3dcd9664e9117657b6c46cb1fea556b95779af1
Siddhantsingh1230 <Siddhantsingh9023134084@gmail.com>
2023-09-06 22:06:48 +0530

    updated Dynamic Content

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -42,7 +42,7 @@ const Content = () => {
                 return <Card post={post} key={i} />;
               })
             ) : (
-              <p className="emptyContent">No Posts</p>
+              <p className="emptyContent">Loading..</p>
             )}
           </div>
         </div>
