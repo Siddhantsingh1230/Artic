69ff6bae64ba512002fd25f768b5b89c67d43882
Siddhantsingh1230 <Siddhantsingh9023134084@gmail.com>
2023-09-06 11:43:28 +0530

    comment user dp fetch

--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -1,11 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { serverURI } from "../App";
 
 const Comments = ({comment}) => {
+  const [profilePhoto,setProfilePhoto] = useState("");
+  const fetchProfilePhoto = async ()=>{
+    try {
+      const { data } = await axios.get(
+        `${serverURI}/read/${comment.profileImageURL}`,
+        {
+          withCredentials: true,
+        }
+      );
+      setProfilePhoto(data.fileUrl);
+    } catch (error) {
+      console.log("error");
+    }
+  }
+  useEffect(()=>{
+    fetchProfilePhoto();
+  },[]);
   return (
     <>
       <div className="comment">
         <div className="commentUser">
-          <img src={comment.userProfileURL} alt="" />
+          <img src={profilePhoto} alt="" />
           <p>{comment.userName}</p>
           <p>•</p>
         </div>
