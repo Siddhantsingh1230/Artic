dbdade4dd8cd20b1dadb08ca0b1de921af0c627c
Siddhantsingh1230 <Siddhantsingh9023134084@gmail.com>
2023-09-06 11:43:52 +0530

    comment user dp fetch

--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { serverURI } from "../App";
+import axios from "axios";
 
 const Comments = ({comment}) => {
   const [profilePhoto,setProfilePhoto] = useState("");
