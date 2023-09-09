2139945689364fbce26eed4fc05a0b1f9b4b2ca6
Siddhantsingh1230 <Siddhantsingh9023134084@gmail.com>
2023-09-09 10:18:13 +0530

    added forgot pwd and resetpwd pages

--- src/components/ForgotPwd.jsx
+++ src/components/ForgotPwd.jsx
@@ -1,34 +1,71 @@
 import { useState } from "react";
+import axios from "axios";
+import { serverURI } from "../App";
+import toast from "react-hot-toast";
+import Spinner from "./Spinner";
+
 const ForgotPassword = () => {
-    const [email, setEmail] = useState("");
-    return (
-        <div className="resetDiv">
-            <div className="resetMainDiv">
-                <img src="/icon/Logo.png" alt="logo" className="logo" />
-                <h2>Forgot Password</h2>
-                <div className="resetPwdDiv">
-                    <p>Enter your user account's verified email addresss and we will send you a password resent link.</p>
-                    <div className="inputDiv email">
-                        <input
-                        autoComplete="email"
-                        type="email"
-                        placeholder="Email"
-                        name="email"
-                        required
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                        ></input>
-                        <i
-                        className="fa-solid fa-envelope fa-xs"
-                        style={{ color: "#f4f5f6" }}
-                        ></i>
-                    </div>
+  const [email, setEmail] = useState("");
+  const [loading, setLoading] = useState(false);
+  const sendMail = async () => {
+    if (email.trim().length === 0) {
+      return toast.error("Email is empty");
+    }
+    try {
+      setLoading(true);
+      const { data } = await axios.post(
+        `${serverURI}/factory/forgotpassword`,
+        { email: email.trim()},
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+          withCredentials: true,
+        }
+      );
+      toast.success(data.message);
+      setLoading(false);
+    } catch (error) {
+      if (error.response.data) {
+        toast.error(error.response.data.message);
+      }
+      setLoading(false);
+    }
+  };
+  return (
+    <div className="resetDiv">
+      <div className="resetMainDiv">
+        <img src="/icon/Logo.png" alt="logo" className="logo" />
+        <h2>Forgot Password</h2>
+        <div className="resetPwdDiv">
+          <p>
+            Enter your user account's verified email addresss and we will send
+            you a password resent link.
+          </p>
+          <div className="inputDiv email">
+            <input
+              autoComplete="email"
+              type="email"
+              placeholder="Email"
+              name="email"
+              required
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            ></input>
+            <i
+              className="fa-solid fa-envelope fa-xs"
+              style={{ color: "#f4f5f6" }}
+            ></i>
+          </div>
 
-                    <button className="resetPwdBtn">Send password reset email</button>
-                </div>
-            </div>
+          <button onClick={sendMail} className="resetPwdBtn">
+            Send password reset email
+            {loading && <Spinner />}
+          </button>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default ForgotPassword
+export default ForgotPassword;
